58b886a2c649a74146e7728a3fb7d6c6232585c7
Hama Pashazadeh <pashazad@msu.edu>
2025-01-04 15:25:51 -0500

    add Form and General components for user input

---
+++ src/components/Form.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import General from './General.jsx'
+import '../index.css'
+
+function Form() {
+
+    return (
+        <>
+            <h1>Please fill out the input spaces below.</h1>
+
+            <General/>
+        </>
+    )
+}
+
+export default Form
---
+++ src/components/General.jsx
@@ -0,0 +1,15 @@
+
+
+
+function General() {
+
+    return (
+        <>
+        <input type="text" value={"Name"}/>       
+        <input type="email" value={"Email"}/>       
+        <input type="phone" value={"Phone"}/>       
+        </>
+    )
+}
+
+export default General
